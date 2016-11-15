adad8c503c1c082bce9b598fe6eb8bcdb796e5d8
ozakharov <olegunza@gmail.com>
2016-11-15 12:35:38 +0500

    week1_done

--- week1.py
+++ week1.py
@@ -1,13 +1,15 @@
 import pandas
+from collections import Counter
 
 def strip_name(name):
 	if name.find('Miss.') > -1:
-		return name[name.find('Miss')+6:]
-	else:
-		return 'None'
+		return name[name.find('Miss')+6:].split(' ', 1)[0]
+	elif name.find('Mrs') > -1:
+			if name.find('(') > -1:
+				return name[name.find('(')+1:len(name)-1].split(' ', 1)[0]
+		
 
 data = pandas.read_csv('titanic.csv', index_col='PassengerId')
-print(data.head())
 print (data['Sex'].value_counts())
 print('-' * 40)
 perc_survived = data['Survived'].sum() * 100 / data['Survived'].count()
@@ -27,9 +29,11 @@ print(df)
 print('-' * 40)
 females = data.ix[data.Sex == 'female']
 females_names = females['Name']
-print(females_names)
 
+first_names = []
 for i in females_names:
 	pass
 	#print(strip_name(i))
+	first_names.append(strip_name(i))
+print(Counter(first_names))
 
