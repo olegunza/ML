b94f14cd5ced2e1edc0329a537a3376ed8c0dfb9
Oleg Zakharov <olegunza@gmail.com>
2016-11-17 22:38:58 +0500

    binary tree answer

---
+++ week1_binarytree.py
@@ -0,0 +1,12 @@
+import pandas
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+
+data = pandas.read_csv('titanic.csv', index_col='PassengerId')
+
+data_used = data[['Pclass', 'Fare', 'Sex', 'Age', 'Survived']].dropna(axis=0)
+data_Y = data_used['Survived']
+data_X = data_used[['Pclass', 'Fare', 'Sex', 'Age']] 
+
+clf = DecisionTreeClassifier(random_state=241)
+clf.fit(data_X, data_Y)
