bcff6737b793d48fe4c07caac717ad52665c4e2e
ozakharov <olegunza@gmail.com>
2016-11-18 14:34:54 +0500

    week1_done

--- week1_binarytree.py
+++ week1_binarytree.py
@@ -7,6 +7,10 @@ data = pandas.read_csv('titanic.csv', index_col='PassengerId')
 data_used = data[['Pclass', 'Fare', 'Sex', 'Age', 'Survived']].dropna(axis=0)
 data_Y = data_used['Survived']
 data_X = data_used[['Pclass', 'Fare', 'Sex', 'Age']] 
-
+data_X = data_X.replace({'male': 1, 'female': 2})
 clf = DecisionTreeClassifier(random_state=241)
 clf.fit(data_X, data_Y)
+
+importances = clf.feature_importances_
+
+print(importances)
